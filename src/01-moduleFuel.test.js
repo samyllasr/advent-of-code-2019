f78e38251c8ec510e07f307022adc98491aeed46
Samylla <samyllasr@gmail.com>
2019-12-02 21:52:09 -0300

    test deep fuel calculation

--- src/01-moduleFuel.test.js
+++ src/01-moduleFuel.test.js
@@ -1,4 +1,4 @@
-const calculateFuel = require("./01-moduleFuel");
+const { calculateFuel, deepFuel } = require("./01-moduleFuel");
 const fs = require('fs');
 
 test("calculate required fuel", () => {
@@ -8,7 +8,7 @@ test("calculate required fuel", () => {
     expect(calculateFuel(100756)).toBe(33583);
 })
 
-test("calculates from file inputs", (done) => {
+test("calculates from file input", (done) => {
     fs.readFile(__dirname + '/inputs/fuel-inputs.txt', (err, data) => {
         
         const sumFuels = data.toString()
@@ -19,6 +19,28 @@ test("calculates from file inputs", (done) => {
             }, 0)
 
         expect(sumFuels).toBe(3087896);
+
+        done();
+    })
+})
+
+test("deep fuel calculation", () => {
+    expect(deepFuel(14)).toBe(2);
+    expect(deepFuel(1969)).toBe(966);
+    expect(deepFuel(100756)).toBe(50346);
+});
+
+test("calculates deep fuel from file input", (done) => {
+    fs.readFile(__dirname + '/inputs/fuel-inputs.txt', (err, data) => {
+        
+        const sumFuels = data.toString()
+            .trim()    
+            .split('\n')
+            .reduce((acc, m) => {
+                return acc + deepFuel(m);
+            }, 0)
+
+        expect(sumFuels).toBe(4628989);
 
         done();
     })
