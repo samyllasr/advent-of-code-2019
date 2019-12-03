bfebb537e928e42e5c744a6ffca9595912301d49
Samylla <samyllasr@gmail.com>
2019-12-02 21:51:31 -0300

    calculate deep fuel

--- src/01-moduleFuel.js
+++ src/01-moduleFuel.js
@@ -2,4 +2,17 @@ const calculateFuel = (mass) => {
     return Math.floor(mass / 3) - 2;
 };
 
-module.exports = calculateFuel;
+const deepFuel = (mass) => {
+    const fuel = calculateFuel(mass);
+
+    if (fuel <= 0) {
+        return 0;
+    }
+
+    return fuel + deepFuel(fuel);
+};
+
+module.exports = {
+    calculateFuel,
+    deepFuel,
+};
